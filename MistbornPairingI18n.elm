50dada4792e4f4b3ef66ceece0522458fffa38bd
Zhang Yichao <echaozh@gmail.com>
2016-06-02 16:55:54 +0800

    i18n

---
+++ MistbornPairingI18n.elm
@@ -0,0 +1,79 @@
+module MistbornPairingI18n exposing ( Model, Msg, init, update, view )
+
+import Array
+import Json.Decode exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Maybe
+import Process
+import Time
+import Task
+
+type alias Translations = { backButton : String
+                          , symbolIntro : Array.Array String
+                          }
+type alias Model = { lang : String
+                   , translations : Translations
+                   }
+
+type Msg = Fetch String
+         | FetchSucceed Translations
+         | FetchFail String Http.Error
+
+init : String -> (Model, Cmd Msg)
+init lang = ( { lang = lang
+              , translations = { backButton = "<<"
+                               , symbolIntro = Array.empty
+                               }
+              }
+            , fetch lang
+            )
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Fetch lang -> ( model, fetch lang )
+        FetchSucceed translations ->
+            ( { model | translations = translations }, Cmd.none )
+        FetchFail lang err ->
+            let msg = always <| Fetch lang
+            in ( model
+               , Task.perform msg msg <| Process.sleep <| 5 * Time.second
+               )
+                
+view : Model -> Maybe Int -> Html Msg
+view model symbol =
+    let padding = style [ ( "padding", "0 20px" ) ]
+    in div [ style [ ( "z-index", "100" )
+                   , ( "position", "absolute" )
+                   , ( "width", "100%" )
+                   ]
+           ] [ a [ href "index.html"
+                 , padding ] [ text model.translations.backButton ]
+             , a [ href "javascript:void()"
+                 , padding
+                 , onClick <| Fetch "en"
+                 ] [ text "English" ]
+             , text "|"
+             , a [ href "javascript:void()"
+                 , padding
+                 , onClick <| Fetch "zh"
+                 ] [ text <| "中文" ]
+             , h2 [ style [ ( "color", "blue" )
+                          , ( "text-align", "center" )
+                          ]
+                  ] [ text
+                      <| Maybe.withDefault ""
+                      <| flip Array.get model.translations.symbolIntro
+                      <| Maybe.withDefault -1 symbol
+                    ]
+             ]
+
+fetch : String -> Cmd Msg
+fetch lang = Task.perform (FetchFail lang) FetchSucceed
+                <| flip Http.get ("mistborn-pairing." ++ lang ++ ".json")
+                <| object2 Translations
+                    ("backButton" := string)
+                    ("symbolIntro" := array string)
